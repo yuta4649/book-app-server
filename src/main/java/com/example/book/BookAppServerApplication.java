9a1c8f9f023d37f985dca2f9e6f6c7c6dff9a796
YutaMori <yuta46de49@gmail.com>
2021-11-04 21:57:37 +0900

    first commit

---
+++ src/main/java/com/example/book/BookAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookAppServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/book/BookAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
